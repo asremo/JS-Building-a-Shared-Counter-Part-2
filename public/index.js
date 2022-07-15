007e52e6388d933224e4ddb4ff4de4e01432cc09
Amanda Remo <amandaremo@ip-192-168-86-42.ec2.internal>
2022-07-14 20:56:33 -0400

    finished commit

--- public/index.js
+++ public/index.js
@@ -1,13 +1,13 @@
 async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
+    const confirmButton = document.querySelector('#confirm-button');
     const decrementButton = document.querySelector('#decrement-button');
-
+    const inputField = document.querySelector('#user-init-value-setup');
     const response = await fetch('http://localhost:9001/counter');
-
     const result = await response.json();
-    
     let countValue = result.value;
+    let userInputValue = 0;
 
     function increment(){
         countValue++;
@@ -19,8 +19,28 @@ async function main(){
         countContainer.textContent = countValue;
     }
 
+    function handleInput(e) {
+        userInputValue = e.target.value;
+    }
+
+    async function handleConfirm() {
+        const response2 = await fetch('http://localhost:9001/counter', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                value: Number(userInputValue),
+            }),
+        });
+        main();
+    }
+
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
+    inputField.addEventListener('input', handleInput)
+    confirmButton.addEventListener('click', handleConfirm)
+
     countContainer.textContent = countValue;
 }
 main()
